bf689064ecb1de10023bff67285e0c3717868620
Kanan Cassio dos Santos Bernardi <kanancsb@gmail.com>
2023-05-30 09:23:15 -0300

    Create the FireWheel Spell

---
+++ Spells/FireWheel.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireWheel : MonoBehaviour
+{
+    public FireWheelSpell fireWheelSpell;
+    public Rigidbody2D rigidbody;
+    public PlayerHealth currentHealth;
+    public float detectionRange = 5f;
+
+    private Transform target;
+    private List<Transform> hitEnemies = new List<Transform>();
+
+    void Start()
+    {
+        fireWheelSpell = FindObjectOfType<FireWheelSpell>();
+        rigidbody.velocity = transform.right * fireWheelSpell.projectileSpeed;
+        currentHealth = FindObjectOfType<PlayerHealth>();
+        StartCoroutine(ProjectileFade());
+    }
+
+    void FixedUpdate()
+    {
+        if (target != null && !hitEnemies.Contains(target))
+        {
+            Vector2 direction = target.position - transform.position;
+            rigidbody.velocity = direction.normalized * fireWheelSpell.projectileSpeed;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Enemy enemy = collision.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            hitEnemies.Add(enemy.transform);
+
+            if (fireWheelSpell.LifeSteal != 0)
+            {
+                float lifeStealAmount = fireWheelSpell.damage * fireWheelSpell.LifeSteal;
+                enemy.TakeDamage(fireWheelSpell.damage);
+                currentHealth.currentHealth += lifeStealAmount;
+            }
+            else
+            {
+                enemy.TakeDamage(fireWheelSpell.damage);
+            }
+
+            if (target == null || hitEnemies.Contains(target))
+            {
+                target = FindClosestEnemy();
+            }
+        }
+    }
+
+    public Transform FindClosestEnemy()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        Transform closestEnemy = null;
+        float closestDistance = Mathf.Infinity;
+
+        foreach (GameObject enemy in enemies)
+        {
+            if (!hitEnemies.Contains(enemy.transform))
+            {
+                float distance = Vector2.Distance(transform.position, enemy.transform.position);
+                if (distance <= detectionRange && distance < closestDistance)
+                {
+                    closestEnemy = enemy.transform;
+                    closestDistance = distance;
+                }
+            }
+        }
+
+        return closestEnemy;
+    }
+
+    IEnumerator ProjectileFade()
+    {
+        yield return new WaitForSeconds(5f);
+        Destroy(gameObject);
+    }
+}
---
+++ Spells/FireWheelSpell.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireWheelSpell : MonoBehaviour
+{
+    // Reference to the position where the spell will be cast from
+    public Transform SpellPosition;
+
+    // Reference to the projectile game object
+    public GameObject projectile;
+
+    // Cooldown between spell casts
+    public float CastCD = 3.0f;
+
+    // Damage inflicted by the spell
+    public float damage = 10f;
+
+    // Speed of the projectile
+    public float projectileSpeed = 6f;
+
+    // Time of the last spell cast
+    private float lastCast;
+
+    public float LifeSteal;
+
+    void Update(){
+        // Check if the left mouse button is pressed and enough time has passed since the last cast
+        if (Input.GetMouseButtonDown(1) && Time.time - lastCast >= CastCD){
+            // Calculate the direction from the spell position to the mouse position
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 direction = (mousePosition - SpellPosition.position).normalized;
+
+            // Calculate the angle of the direction in degrees
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            // Create a new projectile at the spell position with the calculated angle
+            GameObject newProjectile = Instantiate(projectile, SpellPosition.position, Quaternion.AngleAxis(angle, Vector3.forward));
+
+            // Set the time of the last cast to the current time
+            lastCast = Time.time;
+        }
+    }
+}
